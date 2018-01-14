75c6043e643184232e2ac44ff1e414cb88888975
ovsoil <hxyumail@126.com>
2018-01-14 09:00:04 +0800

    7. Reverse Integer

---
+++ src/7. Reverse Integer/TEST.cpp
@@ -0,0 +1,15 @@
+#include "../../framework/header.hpp"
+#include "../../framework/common.hpp"
+#include "solution.hpp"
+
+
+TEST_CASE( "Testcase for Reverse Integer", "[interger]" ) {
+    Solution s;
+
+    REQUIRE( s.reverse(123) == 321 );
+    REQUIRE( s.reverse(-123) == -321 );
+    REQUIRE( s.reverse(120) == 21 );
+    REQUIRE( s.reverse(1234567899) == 0 );
+    REQUIRE( s.reverse(2147483647) == 0 );
+    REQUIRE( s.reverse(-2147483648) == 0 );
+}
