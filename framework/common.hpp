2724dd73b9af9b879f203f698df05ed6d8af2d87
ovsoil <hxyumail@126.com>
2020-06-01 08:38:55 +0800

    add json support

--- framework/common.hpp
+++ framework/common.hpp
@@ -7,12 +7,21 @@
 #include <iostream>
 #include <stack>
 #include <algorithm>
+#include "json.hpp"
 using namespace std;
+using json = nlohmann::json;
 
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
 };
 
 ListNode *genList(std::initializer_list<int> vals)
@@ -46,4 +55,55 @@ void printList(ListNode *l)
     cout << ']' << endl;
 }
 
+TreeNode* gen_tree(vector<int> input, TreeNode *node, int i, int n)
+{
+    if (i >= n || input[i] == INT_MIN)
+        return nullptr;
+    node = new TreeNode(input[i]);
+    node->left = gen_tree(input, node->left, 2 * i + 1, n);
+    node->right = gen_tree(input, node->right, 2 * i + 2, n);
+    return node;
+}
+
+TreeNode* gen_binary_tree(vector<int> input)
+{
+    if (input.empty())
+        return nullptr;
+    TreeNode *root = nullptr;
+    return gen_tree(input, root, 0, input.size() - 1);
+}
+
+TreeNode* gen_b_tree(vector<int> input)
+{
+    if (input.empty())
+        return nullptr;
+    TreeNode *root = nullptr;
+    return gen_tree(input, root, 0, input.size() - 1);
+}
+
+void preorder(TreeNode* root, vector<int>& result)
+{
+    if (root == nullptr)
+        return;
+    result.push_back(root->val);
+    preorder(root->left, result);
+    preorder(root->right, result);
+}
+
+void inorder(TreeNode* root, vector<int>& result) 
+{
+    if (root == nullptr)
+        return;
+    inorder(root->left, result);
+    result.push_back(root->val);
+    inorder(root->right, result);
+}
+
+void print_json()
+{
+    json j = "{\"x\":[null,3,7,true,9,true,15,false,20,false]}"_json;
+    cout << j["x"].size() << endl;
+    string s = j.dump();
+    cout << s << endl;
+}
 #endif //_COMMON_H_
